1be4b6b0ffd98cdc4aea2967cea91650d8b0189d
bk <naturalbb360@gmail.com>
2024-11-07 01:55:34 +0900

    feat(Application): 파일에서 읽은 데이터로 상품과 프로모션 객체 생성

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,26 @@
 package store;
 
+import java.io.IOException;
+import java.util.List;
+import utill.FileReader;
+
 public class Application {
+
+    public final static String productsMdDirectory = "src/main/resources/products.md";
+    public final static String promotionsMdDirectory = "src/main/resources/promotions.md";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            List<String> promotionInfos = FileReader.parseMdFile(promotionsMdDirectory);
+            promotionInfos.removeFirst();
+            List<String> productInfos = FileReader.parseMdFile(productsMdDirectory);
+            productInfos.removeFirst();
+            Promotions promotions = new Promotions(MdFilePromotionInfoParser.parseAll(promotionInfos));
+            List<Product> products = MdFileProductInfoParser.parseAll(productInfos, promotions);
+
+            products.forEach(System.out::println);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
