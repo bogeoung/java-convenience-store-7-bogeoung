01e87722a78fa17d1e4ff1bb193450cd0337fdb2
bk <naturalbb360@gmail.com>
2024-11-08 23:13:07 +0900

    test(InputValidatorTest): 리팩토링 및 내용 검증 기능 삭제

--- src/test/java/validator/InputValidatorTest.java
+++ src/test/java/validator/InputValidatorTest.java
@@ -1,38 +1,40 @@
 package validator;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import java.util.ArrayList;
+import java.util.Date;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import store.ErrorMessage;
+import store.Application;
+import store.Product;
+import store.Promotion;
 
 public class InputValidatorTest {
 
-    @ParameterizedTest(name = "{0}가 입력된경우")
-    @ValueSource(strings = {"[사이다 2]", "사이다-2", "[사이다.2], [사이다-2],[감자칩", "[사이다]"})
-    void 입력된_구매상품이_입력형식에_맞지않을경우_예외문구가_출력된다(String candidate) {
-        assertThatThrownBy(() -> InputValidator.validateInputFormat(candidate))
-                .hasMessage(ErrorMessage.INPUT_FORMAT_IS_WRONG.getErrorMessage());
-    }
+    @BeforeAll
+    static void setUp() {
+        Date dateStart = new Date(2023, 01, 01);
+        Date dateEnd = new Date(2023, 02, 01);
+        Application.products = new ArrayList<>();
+        Promotion promotionSample = new Promotion("MD추천상품", 1, 1, dateStart, dateEnd);
+        Application.products.add(new Product("사이다", 1000, 10, null));
+        Application.products.add(new Product("사이다", 1000, 10, promotionSample));
 
-    @ParameterizedTest(name = "{0}가 입력된경우")
-    @ValueSource(strings = {"[사이다-2]", "[사이다-2],[감자칩-1]", "[안파는항목-1]"})
-    void 입력된_구매상품이_입력형식에_맞는경우_예외문구가_출력되지_않는다(String candidate) {
-        assertDoesNotThrow(() -> InputValidator.validateInputFormat(candidate));
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"[사이다-0]", "[사이다- -1]"})
-    void 입력된_구매상품의_개수가_양수가_아닌경우_예외문구가_출력된다(String candidate) {
-        assertThatThrownBy(() -> InputValidator.validateQuantity(candidate)).hasMessage(
-                ErrorMessage.NOT_ENOUGH_QUANTITY.getErrorMessage());
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"[사이다-1]", "[사이다-100]"})
-    void 입력된_구매상품의_개수가_양수가_아닌경우_예외문구가_출력된다(String candidate) {
-        assertDoesNotThrow(() -> InputValidator.validateQuantity(candidate));
+    @ValueSource(strings = {"[사이다-사이다]", "[사이다--1]"})
+    void 구한_판매상품의_개수가_숫자가_아니라면_false가_리턴된다(String candidate) {
+        assertFalse(InputValidator.validateNumber(candidate));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"사이다, 0", "사이다, 10000"})
+    void 구한_판매상품의_개수가_0이거나_재고가부족하면_false가_리턴된다(String candidateName, String candidateQuantity) {
+        assertFalse(InputValidator.validateQuantity(candidateName, candidateQuantity));
     }
 
 }
