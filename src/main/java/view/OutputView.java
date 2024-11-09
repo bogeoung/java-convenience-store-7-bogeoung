adb18b12e2075dd1d76d28dac3a52f6ddb5c321a
bk <naturalbb360@gmail.com>
2024-11-10 03:45:45 +0900

    feat(OutputView): 출력 메세지 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,54 +1,76 @@
 package view;
 
 import java.util.List;
+import store.ErrorMessage;
 import store.Product;
-import store.Promotion;
 
 public class OutputView {
 
     private static final String START_PURCHASE_COMMENT = "안녕하세요. W편의점입니다. \n현재 보유하고 있는 상품입니다.\n";
-    public static final String PRODUCT_START_SIGN = "- ";
-    public static final String PRODUCT_QUANTITY_UNIT = "%d개";
-    public static final String PRODUCT_PRICE_UNIT = "%,d원";
-    public static final String PRODUCT_QUANTITY_ZERO = "재고 없음";
+    private static final String PRODUCT_START_SIGN = "- ";
+    private static final String PRODUCT_QUANTITY_UNIT = "%d개";
+    private static final String PRODUCT_PRICE_UNIT = "%,d원";
+    private static final String PRODUCT_QUANTITY_ZERO = "재고 없음";
+    private static final String RECEIPT_START_MESSAGE = "==============W 편의점================\n";
+    private static final String RECEIPT_ITEM_MESSAGE = "상품명\t\t수량\t금액";
+    private static final String RECEIPT_BONUS_MESSAGE = "=============증\t정===============\n";
+    private static final String RECEIPT_TOTAL_MESSAGE = "====================================\n";
+    private static final String RECEIPT_TOTAL_AMOUNT = "총구매액";
+    private static final String RECEIPT_PROMOTION_DISCOUNT = "행사할인";
+    private static final String RECEIPT_MEMBERSHIP_DISCOUNT = "멤버십할인";
+    private static final String RECEIPT_PAY_MONEY = "내실돈";
 
-    public void printStartPurchase(List<Product> products) {
+    public static void printStartPurchase(List<Product> products) {
         System.out.println(START_PURCHASE_COMMENT);
         printProducts(products);
     }
 
-    public void printProducts(List<Product> products) {
+    public static void printProducts(List<Product> products) {
         for (Product product : products) {
             System.out.println(printCurrentProducts(product));
         }
     }
 
-    private String printCurrentProducts(Product product) {
+    public static void printReceipt(List<Product> products) {
+        StringBuilder stringBuffer = new StringBuilder();
+        stringBuffer.append(RECEIPT_START_MESSAGE).append(RECEIPT_START_MESSAGE);
+        for (Product product : products) {
+            stringBuffer.append(product.getName()).append("\t").append("상품 판매개수 추가 예정");
+        }
+        stringBuffer.append(RECEIPT_BONUS_MESSAGE);
+        stringBuffer.append(RECEIPT_TOTAL_MESSAGE);
+        stringBuffer.append(RECEIPT_PROMOTION_DISCOUNT);
+        stringBuffer.append(RECEIPT_MEMBERSHIP_DISCOUNT);
+        stringBuffer.append(RECEIPT_PAY_MONEY);
+        System.out.println(stringBuffer.toString());
+    }
+
+    public static void printProductNotExistMessage() {
+        System.out.println(ErrorMessage.NOT_SELLING_PRODUCT.getErrorMessage());
+    }
+
+    public static void printNotEnoughQuantityMessage() {
+        System.out.println(ErrorMessage.NOT_ENOUGH_QUANTITY.getErrorMessage());
+    }
+
+    private static String printCurrentProducts(Product product) {
         StringBuffer stringBuffer = new StringBuffer();
         stringBuffer.append(PRODUCT_START_SIGN).append(product.getName());
         stringBuffer.append(" ").append(getPrice(product));
         stringBuffer.append(" ").append(getQuantity(product));
-        stringBuffer.append(" ").append(getPromotionName(product));
+        stringBuffer.append(" ").append(product.getPromotionName());
         return stringBuffer.toString();
     }
 
-    private String getPrice(Product product) {
+    private static String getPrice(Product product) {
         return String.format(PRODUCT_PRICE_UNIT, product.getPrice());
     }
 
-    private String getQuantity(Product product) {
+    private static String getQuantity(Product product) {
         int quantity = product.getQuantity();
         if (quantity == 0) {
             return PRODUCT_QUANTITY_ZERO;
         }
         return String.format(PRODUCT_QUANTITY_UNIT, quantity);
-    }
-
-    private String getPromotionName(Product product) {
-        Promotion promotion = product.getPromotion();
-        if (promotion == null) {
-            return "";
-        }
-        return promotion.getName();
     }
 }
