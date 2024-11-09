7a11b095953406dde595793436d11bccffd36a46
bk <naturalbb360@gmail.com>
2024-11-10 03:35:57 +0900

    fix(InputParser): 예외 처리 추가

--- src/main/java/parser/InputParser.java
+++ src/main/java/parser/InputParser.java
@@ -3,28 +3,35 @@ package parser;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
-import store.MdFileProductInfoParser;
 
 public class InputParser {
-
+    
     public static boolean validateInputFormat(String input) {
         return Pattern.matches("^\\[\\s*\\S+\\s*-\\s*\\d+\\s*\\]$", input);
     }
 
-    public static List<String> splitInput(String input) {
-        return Arrays.stream(input.split(MdFileProductInfoParser.PRODUCT_INFO_DELIMITER, -1))
+    public static List<String> splitInput(String input, String delimiter) {
+        return Arrays.stream(input.split(delimiter, -1))
                 .map(String::strip).toList();
     }
 
-    public static String getProductNumber(String input) {
-        String productNumber = input.split("-")[1];
-        productNumber = productNumber.replaceAll("[^0-9]", "");
-        return productNumber.strip();
+    public static int getProductQuantity(String input) throws IllegalArgumentException, NumberFormatException {
+        String[] splitInfo = input.split("-");
+        if (splitInfo.length < 2) {
+            throw new IllegalArgumentException();
+        }
+        String inputproductNumber = splitInfo[1];
+        inputproductNumber = inputproductNumber.replaceAll("[^0-9]", "");
+        return Integer.parseInt(inputproductNumber.strip());
     }
 
-    public static String getProductName(String input) {
+    public static String getProductName(String input) throws IllegalArgumentException {
+        String[] splitInfo = input.split("-");
+        if (splitInfo.length < 1) {
+            throw new IllegalArgumentException();
+        }
         String productName = input.split("-")[0];
-        productName = productName.replaceAll("\\[", "");
+        productName = productName.replaceAll("[^가-힣]", "");
         return productName.strip();
     }
 }
