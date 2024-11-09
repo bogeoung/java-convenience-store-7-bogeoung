361bb16970927f769de80baad8a90edf37475e16
bk <naturalbb360@gmail.com>
2024-11-10 03:37:31 +0900

    feat(Customer): 구매 및 정가 결제 상품의 주문 취소 기능 추가

--- src/main/java/store/Customer.java
+++ src/main/java/store/Customer.java
@@ -1,14 +1,29 @@
 package store;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Customer {
 
-    private final Map<String, Integer> items;
+    private final Map<String, Integer> items = new LinkedHashMap<>();
 
-    public Customer(Map<String, Integer> itemsInput) {
-        this.items = itemsInput;
+    public void purchase(String productName, int quantity) {
+        if (items.containsKey(productName)) {
+            items.put(productName, items.get(productName) + quantity);
+            return;
+        }
+        items.put(productName, quantity);
+    }
+
+    public void cancelPurchase(String productName, int quantity) {
+        if (items.containsKey(productName)) {
+            if (items.get(productName) - quantity < 0) {
+                items.remove(productName);
+                return;
+            }
+            items.put(productName, items.get(productName) - quantity);
+        }
     }
 
     public List<String> getItemName() {
@@ -17,5 +32,16 @@ public class Customer {
 
     public int getItemCount(String itemName) {
         return items.get(itemName);
+    }
+
+    public List<String> getItemNames() {
+        return items.keySet().stream().toList();
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "items=" + items +
+                '}';
     }
 }
