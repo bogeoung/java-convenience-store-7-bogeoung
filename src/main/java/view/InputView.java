bf98367e4b2ea035b9e79a5d50736491f538ad2c
bk <naturalbb360@gmail.com>
2024-11-10 03:34:03 +0900

    feat(InputView): 진행 메시지 및 입력 기능 추가 - 멤버십 할인 적용 여부 입력 기능 추가 - 프로모션 상품 추가 여부 입력 기능 추가 - 프로모션 재고 부족 상품에 대한 구매 여부 입력 기능 추가 - 추가 구매 여부 입력 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,17 +1,53 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class InputView {
 
-    public static final String PURCHASE_PRODUCT_MESSAGE = "\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String PURCHASE_PRODUCT_MESSAGE = "\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String MEMBERSHIP_COMMENT = "멤버십 할인을 받으시겠습니까? (Y/N)";
+    private static final String CONTINUE_PURCHASE_COMMENT = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
+    private static final String INTENTION_NOT_APPLY_PROMOTION = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n";
+    private static final String INTENTION_ADD_PROMOTION_PRODUCT = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n";
 
-    public String startPurchase() {
+    public static String getPurchaseProductAndQuantity() {
         System.out.println(PURCHASE_PRODUCT_MESSAGE);
         return readItem();
     }
 
-    public String readItem() {
+    public static String getMembershipDiscountApply() {
+        System.out.println(MEMBERSHIP_COMMENT);
+        return readItem();
+    }
+
+    public static String continuePurchase() {
+        System.out.println(CONTINUE_PURCHASE_COMMENT);
+        return readItem();
+    }
+
+    public static String getIntentionNotApplyPromotion(String productName, int Quantity) {
+        System.out.printf(INTENTION_NOT_APPLY_PROMOTION, productName, Quantity);
+        return readItem();
+    }
+
+    public static String getIntentionAddPromotionProduct(String productName, int Quantity) {
+        System.out.printf(INTENTION_ADD_PROMOTION_PRODUCT, productName, Quantity);
+        return readItem();
+    }
+
+    public static String readItem() {
         return Console.readLine();
+    }
+
+    public static String getValueUntilValidated(Predicate<String> validate, String errorMessage,
+                                                Supplier<String> supplier) {
+        String value = supplier.get();
+        while (validate.test(value)) {
+            System.out.println(errorMessage);
+            value = supplier.get();
+        }
+        return value;
     }
 }
