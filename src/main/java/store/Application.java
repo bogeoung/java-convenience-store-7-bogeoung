7b81ae398ee169bb20b5986c01aaf3312aabe872
bk <naturalbb360@gmail.com>
2024-11-11 03:13:33 +0900

    refactor(Application): 메소드 분리

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -15,62 +15,18 @@ public class Application {
     public final static String PRODUCT_INFO_DELIMITER = ",";
     public final static Predicate<String> YES_OR_NO_INPUT_VALIDATE = (value) -> !List.of("Y", "N").contains(value);
     public final static String YES_OR_NO_INPUT_ERROR_MESSAGE =
-            ErrorMessage.INPUT_FORMAT_IS_WRONG.getErrorMessage();
+            ErrorMessage.INPUT_FORMAT_IS_WRONG.getMessage();
 
     public static void main(String[] args) {
         List<Product> products = loadProductInfos();
         ConvenienceStore convenienceStore = new ConvenienceStore(products);
-        while (true) {
+        do {
             OutputView.printStartPurchase(products);
             Customer customer = getPurchaseProducts(convenienceStore, products);
             purchase(convenienceStore, customer);
-            String membershipDiscountApply = InputView.getValueUntilValidated(YES_OR_NO_INPUT_VALIDATE,
-                    YES_OR_NO_INPUT_ERROR_MESSAGE,
-                    InputView::getMembershipDiscountApply);
-            List<PurchaseInfo> purchaseInfos = customer.getItemNames().stream()
-                    .map(productName -> new PurchaseInfo(productName, customer.getItemCount(productName))).toList();
-            Receipt receipt = convenienceStore.purchase(purchaseInfos, membershipDiscountApply);
-            OutputView.printTotalReceipt(receipt, membershipDiscountApply);
-            String continuous = InputView.getValueUntilValidated(YES_OR_NO_INPUT_VALIDATE,
-                    YES_OR_NO_INPUT_ERROR_MESSAGE,
-                    InputView::continuePurchase);
-            if (continuous.equals("N")) {
-                break;
-            }
-        }
-
-    }
-
-    private static Customer getPurchaseProducts(ConvenienceStore convenienceStore, List<Product> products) {
-        Customer customer = new Customer();
-        boolean isValidated = false;
-        while (!isValidated) {
-            String purchaseProductAndQuantity = InputView.getPurchaseProductAndQuantity();
-            List<String> purchaseProducts = InputParser.splitInput(purchaseProductAndQuantity, PRODUCT_INFO_DELIMITER);
-            for (String purchaseProduct : purchaseProducts) {
-                try {
-                    isValidated = true;
-                    String productName = InputParser.getProductName(purchaseProduct);
-                    int productQuantity = InputParser.getProductQuantity(purchaseProduct);
-                    if (!isValidProductName(products, productName)) {
-                        OutputView.printProductNotExistMessage();
-                        isValidated = false;
-                        break;
-                    }
-                    if (!isValidProductQuantity(convenienceStore, productName, productQuantity)) {
-                        OutputView.printNotEnoughQuantityMessage();
-                        isValidated = false;
-                        break;
-                    }
-                    customer.purchase(productName, productQuantity);
-                } catch (IllegalArgumentException | NullPointerException e) {
-                    System.out.println(ErrorMessage.INPUT_FORMAT_IS_WRONG.getErrorMessage());
-                    isValidated = false;
-                    break;
-                }
-            }
-        }
-        return customer;
+            Receipt receipt = convenienceStore.sellProducts(getPurchaseInfos(customer), getMembershipDiscountApply());
+            OutputView.printTotalReceipt(receipt);
+        } while (!getContinueIntention().equals("N"));
     }
 
     private static List<Product> loadProductInfos() {
@@ -84,6 +40,64 @@ public class Application {
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
+    }
+
+    private static Customer getPurchaseProducts(ConvenienceStore convenienceStore, List<Product> products) {
+        Customer customer = new Customer();
+        boolean isValidated = false;
+        while (!isValidated) {
+            String purchaseProductAndQuantity = InputView.getPurchaseProductAndQuantity();
+            List<String> purchaseProducts = InputParser.splitInput(purchaseProductAndQuantity, PRODUCT_INFO_DELIMITER);
+            for (String purchaseProduct : purchaseProducts) {
+                isValidated = purchaseWhenValidate(convenienceStore, products, purchaseProduct, customer);
+            }
+        }
+        return customer;
+    }
+
+    private static List<PurchaseInfo> getPurchaseInfos(Customer customer) {
+        return customer.getItemNames().stream()
+                .map(productName -> new PurchaseInfo(productName, customer.getItemCount(productName))).toList();
+    }
+
+    private static String getMembershipDiscountApply() {
+        return InputView.getValueUntilValidated(YES_OR_NO_INPUT_VALIDATE,
+                YES_OR_NO_INPUT_ERROR_MESSAGE,
+                InputView::getMembershipDiscountApply);
+    }
+
+    private static String getContinueIntention() {
+        return InputView.getValueUntilValidated(YES_OR_NO_INPUT_VALIDATE,
+                YES_OR_NO_INPUT_ERROR_MESSAGE,
+                InputView::continuePurchase);
+    }
+
+    private static boolean purchaseWhenValidate(ConvenienceStore convenienceStore, List<Product> products,
+                                                String product, Customer customer) {
+        try {
+            if (!validatePurchaseProduct(products, InputParser.getProductName(product), convenienceStore,
+                    InputParser.getProductQuantity(product))) {
+                return false;
+            }
+            customer.purchase(InputParser.getProductName(product), InputParser.getProductQuantity(product));
+        } catch (IllegalArgumentException e) {
+            System.out.println(ErrorMessage.INPUT_FORMAT_IS_WRONG.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean validatePurchaseProduct(List<Product> products, String productName,
+                                                   ConvenienceStore convenienceStore, int productQuantity) {
+        if (!isValidProductName(products, productName)) {
+            OutputView.printProductNotExistMessage();
+            return false;
+        }
+        if (!isValidProductQuantity(convenienceStore, productName, productQuantity)) {
+            OutputView.printNotEnoughQuantityMessage();
+            return false;
+        }
+        return true;
     }
 
     private static boolean isValidProductName(List<Product> products, String productName) {
@@ -101,25 +115,37 @@ public class Application {
             int quantity = customer.getItemCount(purchaseItemName);
             int promotionAppliedQuantity = convenienceStore.getPromotionAppliedQuantity(purchaseItemName, quantity);
             if (promotionAppliedQuantity > quantity) {
-                String add = InputView.getValueUntilValidated(
-                        YES_OR_NO_INPUT_VALIDATE,
-                        YES_OR_NO_INPUT_ERROR_MESSAGE,
-                        () -> InputView.getIntentionAddPromotionProduct(purchaseItemName,
-                                promotionAppliedQuantity - quantity));
-                if (add.equals("Y")) {
-                    customer.purchase(purchaseItemName, promotionAppliedQuantity - quantity);
-                }
+                purchasePromotionQuantityIsEnough(customer, purchaseItemName, promotionAppliedQuantity, quantity);
             }
             if (0 < promotionAppliedQuantity && promotionAppliedQuantity < quantity) {
-                String apply = InputView.getValueUntilValidated(
-                        YES_OR_NO_INPUT_VALIDATE,
-                        YES_OR_NO_INPUT_ERROR_MESSAGE,
-                        () -> InputView.getIntentionNotApplyPromotion(purchaseItemName,
-                                quantity - promotionAppliedQuantity));
-                if (apply.equals("N")) {
-                    customer.cancelPurchase(purchaseItemName, quantity - promotionAppliedQuantity);
-                }
+                purchasePromotionQuantityIsNotEnough(customer, purchaseItemName, promotionAppliedQuantity, quantity);
             }
         }
     }
+
+    private static void purchasePromotionQuantityIsEnough(Customer customer, String purchaseItemName,
+                                                          int promotionAppliedQuantity,
+                                                          int quantity) {
+        String add = InputView.getValueUntilValidated(
+                YES_OR_NO_INPUT_VALIDATE,
+                YES_OR_NO_INPUT_ERROR_MESSAGE,
+                () -> InputView.getIntentionAddPromotionProduct(purchaseItemName,
+                        promotionAppliedQuantity - quantity));
+        if (add.equals("Y")) {
+            customer.purchase(purchaseItemName, promotionAppliedQuantity - quantity);
+        }
+    }
+
+    private static void purchasePromotionQuantityIsNotEnough(Customer customer, String purchaseItemName,
+                                                             int promotionAppliedQuantity, int quantity) {
+        String apply = InputView.getValueUntilValidated(
+                YES_OR_NO_INPUT_VALIDATE,
+                YES_OR_NO_INPUT_ERROR_MESSAGE,
+                () -> InputView.getIntentionNotApplyPromotion(purchaseItemName,
+                        quantity - promotionAppliedQuantity));
+        if (apply.equals("N")) {
+            customer.cancelPurchase(purchaseItemName, quantity - promotionAppliedQuantity);
+        }
+    }
+
 }
